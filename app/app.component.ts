9988aa3236193676ac58fcff0d01dacfa12a16d5
mneucollins <mneucollins@gmail.com>
2017-01-18 16:55:45 -0500

    adds starter files

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'aqt-app',
+    template: `
+        <h1>AQT - Angular2 Starter</h1>
+    `
+})
+export class AppComponent { }
