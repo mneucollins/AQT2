cbd60c1cc3eb1cdf73a34f768c40b302b9740390
mneucollins <mneucollins@gmail.com>
2017-01-29 18:55:11 -0500

    WIres up the Home and the Back Global Navigation Buttons

--- app/shared/globalnav.component.js
+++ app/shared/globalnav.component.js
@@ -9,16 +9,28 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
+var common_1 = require('@angular/common');
 var GlobalNavComponent = (function () {
-    function GlobalNavComponent() {
+    //global routing is in the app.module
+    function GlobalNavComponent(_router, _location) {
+        this._router = _router;
+        this._location = _location;
     }
+    ;
+    GlobalNavComponent.prototype.onHome = function () {
+        this._router.navigate(['/home']);
+    };
+    GlobalNavComponent.prototype.onBack = function () {
+        this._location.back();
+    };
     GlobalNavComponent = __decorate([
         core_1.Component({
             selector: 'globalnav',
             templateUrl: 'app/shared/globalnav.component.html',
             styleUrls: ['app/shared/globalnav.component.css']
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [router_1.Router, common_1.Location])
     ], GlobalNavComponent);
     return GlobalNavComponent;
 }());
