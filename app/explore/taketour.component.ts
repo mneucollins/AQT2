012e50af91f3e365caa898fc89ff327c291d1250
Eva Jacobus <ejacobus@users.noreply.github.com>
2017-02-13 07:34:41 -0600

    View Random stub module (#7)
    * Add view_random stub
    
    * Add find_a_name and take_a_tour stubs

---
+++ app/explore/taketour.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    templateUrl: 'app/explore/taketour.component.html',
+    styleUrls: ['app/explore/taketour.component.css']
+})
+
+export class TakeTourComponent{
+    constructor ( private _router: Router){};
+
+}
