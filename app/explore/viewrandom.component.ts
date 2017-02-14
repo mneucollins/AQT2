910e445c76c6935d1996827a019c6218d63922b0
Eva Jacobus <ejacobus@users.noreply.github.com>
2017-02-13 19:27:11 -0600

    Add basic service (not asynchronous)

--- app/explore/viewrandom.component.ts
+++ app/explore/viewrandom.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { PanelService } from '../services/panel.service';
 
 @Component({
     templateUrl: 'app/explore/viewrandom.component.html',
-    styleUrls: ['app/explore/viewrandom.component.css']
+    styleUrls: ['app/explore/viewrandom.component.css'],
+    providers: [PanelService],
 })
 
-export class ViewRandomComponent{
-    constructor ( private _router: Router){};
+export class ViewRandomComponent implements OnInit{
+
+    panel: HTMLImageElement;
+
+    constructor ( private _router: Router, private PanelService: PanelService){};
+
+    ngOnInit(): void {
+        this.getPanel();
+    }
+
+    getPanel(): void {
+        this.panel = this.PanelService.getRandomPanel();
+        //this.PanelService.getRandomPanel().then(panel => this.panel = panel)
+        //this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+      }
 
 }
