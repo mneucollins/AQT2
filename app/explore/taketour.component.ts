1847f3611be5b8953c2f1d8aa5018d29034bdd2e
Eva Jacobus <ejacobus@users.noreply.github.com>
2017-03-15 20:10:30 -0500

    Tour service (#15)
    * open seadragon experiments
    
    * Basic tour demo, tour service sketched out (but not completed, pending UI discussion)

--- app/explore/taketour.component.ts
+++ app/explore/taketour.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TourService } from '../services/tours.service';
+
 
 @Component({
     templateUrl: 'app/explore/taketour.component.html',
-    styleUrls: ['app/explore/taketour.component.css']
+    styleUrls: ['app/explore/taketour.component.css'],
+    providers: [TourService],
+
 })
 
-export class TakeTourComponent{
-    constructor ( private _router: Router){};
+export class TakeTourComponent implements OnInit{
+
+      tour: String;
+
+      constructor ( private _router: Router, private TourService: TourService){};
+
+      ngOnInit(): void {
+          this.getTour();
+      }
+
+      getTour(): void {
+          //this.tour = this.TourService.getDemoTour();
+          //this.PanelService.getRandomPanel().then(panel => this.panel = panel)
+          //this.heroService.getHeroes().then(heroes => this.heroes = heroes);
 
 }
--- app/services/mock-panels.ts
+++ app/services/mock-panels.ts
@@ -6,3 +6,15 @@ export const PANELS: HTMLImageElement[] = [
     { src: 'app/services/mock-img/4109_Block.jpg' } as HTMLImageElement,
     { src: 'app/services/mock-img/4492_Block.jpg' } as HTMLImageElement,
 ];
+
+export const DZPANELS: String[] = [
+  '00001.dzi',
+  '00002.dzi',
+  '00003.dzi',
+  '00004.dzi',
+  '00005.dzi',
+  '00006.dzi',
+  '00007.dzi',
+  '00008.dzi',
+
+]
