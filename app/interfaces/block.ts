04f34105c28faebf2253ec4c36d29733fd249c96
mneucollins <mneucollins@gmail.com>
2017-04-12 21:29:21 -0400

    minor error fixes

--- app/interfaces/block.ts
+++ app/interfaces/block.ts
@@ -3,7 +3,7 @@ import { IPanel } from './panel';
 export interface IBlock {
     blockNum: string;
     imageUrl: string;
-    panels: IPanel;
+    panels: IPanel[];
     //...+other metadata
     //layout?
 }
